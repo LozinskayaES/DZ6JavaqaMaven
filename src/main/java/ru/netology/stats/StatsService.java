5457e32bd22f5c166487133868bdcc32aedc9390
Evgeniya <lozinskaya.ev@gmail.com>
2024-01-08 00:04:03 +0300

    change DZ6JavaqaMaven

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,2 +1,74 @@
-package ru.netology.stats;public class StatsService {
+package ru.netology.stats;
+
+public class StatsService {
+
+    public long sumOfAllSales(long[] sales) {
+        //переменая в которой будет хранится сумма
+        long sum = 0;
+        //бежим по массиву
+        for (int i = 0; i < sales.length; i++) {
+            //добавляем в переменую текущие значение
+            sum = sum + sales[i];
+        }
+        //возвращаем из метода
+        return sum;
+    }
+
+    public long averageSalesPerMonth(long[] sales) {
+        //получить сумму из метода выше
+        long sum = sumOfAllSales(sales);
+
+        //поделить на длину массива и вернуть результат
+        return sum / sales.length;
+    }
+
+    public long maxSales(long[] sales) {
+        // номер месяца с максимальными продажами
+        int maxMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+        // номер месяца с минимальными продажами
+        int minMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+
+    public long monthsSalesBelowAverage(long[] sales) {
+        //количество месяцев, в которых продажи были ниже среднего
+        int months = 0;
+        long averageSales = averageSalesPerMonth(sales);
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < averageSales) {
+                months = months + 1;
+            }
+        }
+        return months;
+    }
+
+    public long monthsSalesAboveAverage(long[] sales) {
+        //количество месяцев, в которых продажи были выше среднего
+        int months = 0;
+        long averageSales = averageSalesPerMonth(sales);
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > averageSales) {
+                months = months + 1;
+            }
+        }
+        return months;
+    }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,2 +1,73 @@
-package ru.netology.stats;public class StatsServiceTest {
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void shouldSumOfAllSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedSum = 180;
+        long result = service.sumOfAllSales(sales);
+
+        Assertions.assertEquals(expectedSum, result);
+
+    }
+
+    @Test
+    public void shouldAverageSalesPerMonth() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedSum = 15;
+        long result = service.averageSalesPerMonth(sales);
+
+        Assertions.assertEquals(expectedSum, result);
+    }
+
+    @Test
+    public void shouldMaxSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long maxMonth = 8;
+        long result = service.maxSales(sales);
+
+        Assertions.assertEquals(maxMonth, result);
+    }
+
+    @Test
+    public void shouldMinSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long minMonth = 9;
+        long result = service.minSales(sales);
+
+        Assertions.assertEquals(minMonth, result);
+    }
+
+    @Test
+    public void shouldMonthsSalesBelowAverage() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long months = 5;
+        long result = service.monthsSalesBelowAverage(sales);
+
+        Assertions.assertEquals(months, result);
+    }
+
+    @Test
+    public void shouldMonthsSalesAboveAverage() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long months = 5;
+        long result = service.monthsSalesAboveAverage(sales);
+
+        Assertions.assertEquals(months, result);
+    }
 }
